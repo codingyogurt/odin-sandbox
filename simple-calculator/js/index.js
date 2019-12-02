ef4aba40ac83007fa6087cc5f0a3c9309017aa39
codingyogurt <28801036+codingyogurt@users.noreply.github.com>
2019-12-02 21:55:03 +0800

    finished one dot feature :D, finished keyboard support :)

--- simple-calculator/js/index.js
+++ simple-calculator/js/index.js
@@ -1,58 +1,142 @@
 const resultText = document.querySelector(".p-result");
 const equationText = document.querySelector(".p-equation");
+const btnDot = document.querySelector(".dot");
 
-document.querySelectorAll(".normal").forEach((btn) =>{
+const normalFunc = ((eContent) => {
+    if (equationText.textContent === "0") {
+        equationText.textContent =
+            eContent;
+    } else {
+        updateEquation(eContent);
+    }
+    if (eContent === ".") {
+        btnDot.disabled = true;
+    }
+});
+
+document.querySelectorAll(".normal").forEach((btn) => {
     btn.addEventListener("click", (e) => {
-        if(equationText.textContent === "0"){
-            equationText.textContent =
-            e.target.textContent;
-        } else {
-            updateEquation(e.target.textContent);
-        }
+        normalFunc(e.target.textContent);
     });
 });
 
+const specialFunc = ((eClassList) => {
+    if (eClassList.contains("ac")) {
+        clearDisplay();
+        btnDot.disabled = false;
+    }
+    if (eClassList.contains("del")) {
+        backSpace();
+    }
+    if (eClassList.contains("fa-divide")) {
+        updateEquation(" / ");
+        btnDot.disabled = false;
+
+    }
+    if (eClassList.contains("fa-times")) {
+        updateEquation(" x ");
+        btnDot.disabled = false;
+
+    }
+    if (eClassList.contains("fa-minus")) {
+        updateEquation(" - ");
+        btnDot.disabled = false;
+
+    }
+    if (eClassList.contains("fa-plus")) {
+        updateEquation(" + ");
+        btnDot.disabled = false;
+
+    }
+    if (eClassList.contains("equal")) {
+        resultText.innerHTML =
+            `<strong>
+        ${parseProblem(equationText.textContent)}
+        </strong>`;
+    }
+});
+
+
+
+// keyboard support
+
+// printable keys
+window.addEventListener("keypress", (e) => {
+    const code = e.charCode;
+
+    if (code >= 48 &&
+        code <= 57 && // 0-9
+        code !== 47
+    ) { // unknown
+        normalFunc(String.fromCharCode(code));
+    }
+
+    if (code === 46) { // dot
+        if (btnDot.disabled === false) {
+            normalFunc(String.fromCharCode(code));
+        }
+    }
+    
+    if (code === 127){ // delete ac
+        clearDisplay();
+        btnDot.disabled = false;   
+    }
+
+    if (code === 43){ // plus
+        updateEquation(" + ");
+        btnDot.disabled = false;
+    }
+
+    if (code === 45){ // minus
+        updateEquation(" - ");
+        btnDot.disabled = false;
+    }
+
+    if (code === 47){ // divide
+        updateEquation(" / ");
+        btnDot.disabled = false;
+    }
+
+    if (code === 42){ // times
+        updateEquation(" x ");
+        btnDot.disabled = false;
+    }
+
+    if (code === 61 || code === 13){ // equal or enter
+        resultText.innerHTML =
+        `<strong>
+        ${parseProblem(equationText.textContent)}
+        </strong>`;
+    }
+
+});
+// non-printable keys
+window.addEventListener("keydown", (e) => {
+    if(e.code === "Backspace"){
+        backSpace();
+    }
+});
+
+
+
 document.querySelectorAll(".t").forEach((btn) => {
     btn.addEventListener("click", (e) => {
-        if(btn.classList.contains("ac")){
-            clearDisplay();
-        }
-        if(btn.classList.contains("del")){
-            backSpace();
-        }
-        if(btn.classList.contains("fa-divide")){
-            updateEquation(" / ");
-        }
-        if(btn.classList.contains("fa-times")){
-            updateEquation(" x ");
-        }
-        if(btn.classList.contains("fa-minus")){
-            updateEquation(" - ");
-        }
-        if(btn.classList.contains("fa-plus")){
-            updateEquation(" + ");
-        }
-        if(btn.classList.contains("equal")){
-            resultText.innerHTML =
-            `<strong>
-            ${parseProblem(equationText.textContent)}
-            </strong>`; 
-        }
+        specialFunc(btn.classList);
     });
 
 });
 
 const checkIfOperator = ((stringOperator) => {
-    if(stringOperator.indexOf("x") >= 0){
+    if (stringOperator.indexOf("x") >= 0) {
         return true;
     }
-    if(stringOperator.indexOf("/") >= 0){
+    if (stringOperator.indexOf("/") >= 0) {
         return true;
     }
-    if(stringOperator.indexOf("+") >= 0){
+    if (stringOperator.indexOf("+") >= 0) {
         return true;
     }
-    if(stringOperator.indexOf("-") >= 0){
+    if (stringOperator.indexOf("-") >= 0) {
         return true;
     }
 
@@ -62,50 +146,60 @@ const checkIfOperator = ((stringOperator) => {
 const updateEquation = ((stringToAdd) => {
     const text = equationText.textContent;
     let endText = text.substr(text.length - 2, 2);
-    if (checkIfOperator(endText) && checkIfOperator(stringToAdd)){
+    if (checkIfOperator(endText) && checkIfOperator(stringToAdd)) {
         endText = text.substr(0, text.length - 3);
         equationText.textContent = endText + stringToAdd;
     } else {
         equationText.textContent = text + stringToAdd;
     }
-
 });
 
 const backSpace = () => {
     let text = equationText.textContent;
 
-    if(text.substr(text.length - 1, text.length) === " "){
+    if (text.indexOf(".") >= 0) {
+        btnDot.disabled = true;
+    }
+
+    if (text.substr(text.length - 1, text.length) === " ") {
         text = text.substr(0, text.length - 3);
     } else {
-        text = text.substr(0,text.length - 1);
+        text = text.substr(0, text.length - 1);
     }
 
     equationText.textContent = text;
 
-    if(equationText.textContent === ""){
+    let dotCheck = text.substr(text.length - 3, 3);
+    if (checkIfOperator(dotCheck)) {
+        btnDot.disabled = false;
+    }
+
+    if (equationText.textContent === "") {
         equationText.textContent = "0";
-        resultText.textContent = "0";
+        resultText.innerHTML =
+            `<strong>0</strong>`;
+        btnDot.disabled = false;
     }
 }
 
 const clearDisplay = () => {
     equationText.textContent = "0";
     resultText.innerHTML =
-    `<strong>0</strong>`; 
+        `<strong>0</strong>`;
 }
 
 const parseProblem = ((stringProblem) => {
     // check for syntax error
     let endText = stringProblem.substr(stringProblem.length - 2, 2);
-    if(checkIfOperator(endText)){
+    if (checkIfOperator(endText)) {
         return "OOPS!";
     }
 
     // check for dividing by 0
     endText = stringProblem.substr(stringProblem.length - 3, 3);
-    if(endText.indexOf("/") >= 0){
+    if (endText.indexOf("/") >= 0) {
         endText = stringProblem.substr(stringProblem.length - 2, 2);
-        if(endText.indexOf("0") >= 0){
+        if (endText.indexOf("0") >= 0) {
             return "NOPE!";
         }
     }
@@ -114,47 +208,47 @@ const parseProblem = ((stringProblem) => {
 
     let num1, num2;
     // MDAS (M)
-    while(arrayProblem.indexOf("x") !== -1){
+    while (arrayProblem.indexOf("x") !== -1) {
         const xIndex = arrayProblem.indexOf("x");
-        num1 = arrayProblem[xIndex -1];
+        num1 = arrayProblem[xIndex - 1];
         num2 = arrayProblem[xIndex + 1];
         arrayProblem.splice(
             xIndex - 1,
             3,
-            multiply(num1,num2)
+            multiply(num1, num2)
         );
     }
     // (D)
-    while(arrayProblem.indexOf("/") !== -1){
+    while (arrayProblem.indexOf("/") !== -1) {
         const xIndex = arrayProblem.indexOf("/");
-        num1 = arrayProblem[xIndex -1];
+        num1 = arrayProblem[xIndex - 1];
         num2 = arrayProblem[xIndex + 1];
         arrayProblem.splice(
             xIndex - 1,
             3,
-            divide(num1,num2)
+            divide(num1, num2)
         );
     }
     // (A)
-    while(arrayProblem.indexOf("+") !== -1){
+    while (arrayProblem.indexOf("+") !== -1) {
         const xIndex = arrayProblem.indexOf("+");
-        num1 = arrayProblem[xIndex -1];
+        num1 = arrayProblem[xIndex - 1];
         num2 = arrayProblem[xIndex + 1];
         arrayProblem.splice(
             xIndex - 1,
             3,
-            add(num1,num2)
+            add(num1, num2)
         );
     }
     // (S)
-    while(arrayProblem.indexOf("-") !== -1){
+    while (arrayProblem.indexOf("-") !== -1) {
         const xIndex = arrayProblem.indexOf("-");
-        num1 = arrayProblem[xIndex -1];
+        num1 = arrayProblem[xIndex - 1];
         num2 = arrayProblem[xIndex + 1];
         arrayProblem.splice(
             xIndex - 1,
             3,
-            substract(num1,num2)
+            substract(num1, num2)
         );
     }
 
@@ -162,17 +256,17 @@ const parseProblem = ((stringProblem) => {
 });
 
 const operate = ((operator, num1, num2) => {
-    if(operator === "x"){ // MDAS
-        return multiply(num1,num2);
+    if (operator === "x") { // MDAS
+        return multiply(num1, num2);
     }
-    if(operator === "/"){
-        return divide(num1,num2);
+    if (operator === "/") {
+        return divide(num1, num2);
     }
-    if(operator === "+"){
+    if (operator === "+") {
         return add(num1, num2);
     }
-    if(operator === "-"){
-        return substract(num1,num2);
+    if (operator === "-") {
+        return substract(num1, num2);
     }
 });
 
