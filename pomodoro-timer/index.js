1fe7b3ffb116421380a5e5155eb79f2d7cba7885
ranman09 <28801036+ranman09@users.noreply.github.com>
2019-12-06 15:29:33 +0800

    added more UI codes for pomodoro-timer

--- pomodoro-timer/index.js
+++ pomodoro-timer/index.js
@@ -5,14 +5,41 @@ const playBtn = document.querySelector(".play");
 const settingsBtnStart = document.querySelector(".start-scene .edit");
 const settingsBtnSettings = document.querySelector(".settings-scene .edit");
 const settingsBtnMain = document.querySelector(".main-scene .edit");
+const stopBtn = document.querySelector(".stop");
+const pauseBtn = document.querySelector(".pause");
+const adjustBtn = document.querySelector(".adjust");
+const sessionValue = document.querySelector(".session-value");
+const minusBtns = document.querySelectorAll(".minus");
+const plusBtns = document.querySelectorAll(".plus");
 
 playBtn.addEventListener("click", showMain);
 
 settingsBtnStart.addEventListener("click", showSettings);
 
-settingsBtnSettings.addEventListener("click", showStart);
+settingsBtnSettings.addEventListener("click", closeSettings);
 
 settingsBtnMain.addEventListener("click", showSettings);
+
+stopBtn.addEventListener("click", showStart);
+
+pauseBtn.addEventListener("click", (e) => {
+    e.target.classList.toggle("fa-pause");
+    e.target.classList.toggle("fa-play");
+});
+
+adjustBtn.addEventListener("click", showStart);
+
+minusBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        settingChangeValue(btn.nextElementSibling, "minus");
+    })
+});
+
+plusBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        settingChangeValue(btn.previousElementSibling, "plus");
+    })
+});
 
 function showStart() {
     mainScene.classList.remove("fade");
@@ -21,17 +48,39 @@ function showStart() {
 }
 
 function showSettings(){
-    mainScene.classList.remove("fade");
-    startScene.classList.remove("fade");
     settingsScene.classList.add("fade");
+}
+
+function closeSettings(){
+    settingsScene.classList.remove("fade");
 }
 
 function showMain(){
     mainScene.classList.add("fade");
     startScene.classList.remove("fade");
     settingsScene.classList.remove("fade");
-
 }
+
+function settingChangeValue(valueTarget, method){
+    let currentValue = Number(valueTarget.textContent);
+    
+    if (currentValue < 25){        
+        if(method === "plus"){
+            currentValue++;
+            valueTarget.textContent = currentValue;
+        }
+    }
+
+    if (currentValue > 0){
+        if(method === "minus"){
+            currentValue--;
+            valueTarget.textContent = currentValue.toString();
+        }
+    }
+}
+
+
+
 
 
 
