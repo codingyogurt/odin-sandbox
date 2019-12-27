5562e7aaa18bb21389ca4e2fbd530c737281cd4a
codingyogurt <28801036+codingyogurt@users.noreply.github.com>
2019-12-27 23:15:58 +0800

    finished version 1 of retaurant app

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,11 +1,16 @@
 import { component as nav } from "./nav.js";
 import { component as homepage } from "./homepage.js";
+import { component as menupage } from "./menu.js";
+import { component as aboutpage } from "./about.js";
 
 const content = document.querySelector("#content");
 
 // setup nav bar
 const showNavBar = () => {
-  document.body.appendChild(nav("RESTAURANT", ["Home", "Menu", "About Us"]));
+  document.body.insertBefore(
+    nav("RESTAURANT", ["Home", "Menu", "About Us"]),
+    document.body.firstChild
+  );
 };
 
 // add listeners to nav buttons, this requires that buttons are loaded via nav
@@ -16,6 +21,12 @@ const addNavListeners = () => {
       switch (a.textContent) {
         case "Home":
           showHomePage();
+          break;
+        case "Menu":
+          showMenuPage();
+          break;
+        case "About Us":
+          showAboutPage();
           break;
       }
     });
@@ -35,6 +46,36 @@ const showHomePage = () => {
   );
   homepage.setBtnText("Check out our menu");
   content.appendChild(homepage.getComponent());
+};
+
+// setup menu page
+const showMenuPage = () => {
+  clearPage();
+  menupage.createMenuItem(
+    "Sinigang na baboy",
+    "https://panlasangpinoy.com/wp-content/uploads/2018/11/Sinigang-na-Baboy-Recipe.jpg"
+  );
+  menupage.createMenuItem(
+    "Crispy Liempo",
+    "https://panlasangpinoy.com/wp-content/uploads/2019/12/turbo-crispy-liempo-recipe.jpg"
+  );
+  menupage.createMenuItem(
+    "Crispy Sisig",
+    "https://panlasangpinoy.com/wp-content/uploads/2019/12/sizzling-crispy-sisig-.jpg"
+  );
+
+  content.appendChild(menupage.getComponent());
+};
+
+// setup about page
+const showAboutPage = () => {
+  clearPage();
+  aboutpage.setText(
+    "Lorem ipsum dolor sit.",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et commodi facere eum fugiat nobis dolorem perspiciatis maiores. Facilis, earum placeat."
+  );
+
+  content.appendChild(aboutpage.getComponent());
 };
 
 // clear contents of the page
